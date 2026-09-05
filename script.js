3784ee4e66a3b6e63d089028778f0c21cc001809
Aravinthlavan <aravinth.r09@outlook.com>
2026-09-05 12:31:31 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector('.menu-btn');
-const nav = document.querySelector('.desktop-nav');
-if (menu && nav) {
-  menu.addEventListener('click', () => {
-    const open = menu.getAttribute('aria-expanded') === 'true';
-    menu.setAttribute('aria-expanded', String(!open));
-    nav.classList.toggle('mobile-open');
-  });
-}
